8b381fe93384a21f0e0bfd99dfc6871266848ae7
emmanuelist <emmanuel.paul75@yahoo.com>
2024-07-22 19:44:43 +0100

    Implement interactive console program to greet users by name

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,8 +1,12 @@
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-process.stdin.on('data', (data) => {
-  const name = data.toString().trim();
-  process.stdout.write(`Your name is: ${name}\n`);
-  process.stdout.write('This important software is now cloasing\n');
-  process.stdin.end();
+process.stdin.on('readable', () => {
+  const name = process.stdin.read();
+  if (name !== null) {
+    process.stdout.write(`Your name is: ${name}`);
+  }
+});
+
+process.stdin.on('end', () => {
+  console.log('This important software is now closing');
 });
