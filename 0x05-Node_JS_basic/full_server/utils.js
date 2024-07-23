a27c912019b72eb74bfeb241e98715f757a40155
emmanuelist <emmanuel.paul75@yahoo.com>
2024-07-23 19:42:48 +0100

    Commit subfiles

--- 0x05-Node_JS_basic/full_server/utils.js
+++ 0x05-Node_JS_basic/full_server/utils.js
@@ -1,19 +1,18 @@
 const fs = require('fs').promises;
 
-const readDatabase = (filePath) =>
-  new Promise((resolve, reject) => {
-    fs.readFile(filePath, 'utf8')
-      .then((data) => {
-        const lines = data.trim().split('\n').slice(1);
-        const students = {};
-        lines.forEach((line) => {
-          const [firstName, , , field] = line.split(',');
-          if (!students[field]) students[field] = [];
-          students[field].push(firstName);
-        });
-        resolve(students);
-      })
-      .catch(() => reject(new Error('Cannot load the database')));
-  });
+const readDatabase = (filePath) => new Promise((resolve, reject) => {
+  fs.readFile(filePath, 'utf8')
+    .then((data) => {
+      const lines = data.trim().split('\n').slice(1);
+      const students = {};
+      lines.forEach((line) => {
+        const [firstName, , , field] = line.split(',');
+        if (!students[field]) students[field] = [];
+        students[field].push(firstName);
+      });
+      resolve(students);
+    })
+    .catch(() => reject(new Error('Cannot load the database')));
+});
 
 module.exports = readDatabase;
